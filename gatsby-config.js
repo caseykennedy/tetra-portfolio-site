89f4c84fa8f4568f2dc6ef8f582f34df3943ec8b
Casey K <tehcnicolour@gmail.com>
2022-11-16 20:17:19 -0800

    remove images from filesystem source

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,13 +87,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'images',
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
       resolve: `gatsby-plugin-sharp`,
       options: {
         defaults: {
