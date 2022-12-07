78c05d7fd1e7a63c903d19ecd80eae0a4b65aac0
Casey K <tehcnicolour@gmail.com>
2022-12-06 16:00:00 -0800

    less grain

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -79,7 +79,7 @@ const GlobalStyles = createGlobalStyle`
     pointer-events: none;
     animation: grain 1s steps(6) infinite;
     background-image: url(//res.cloudinary.com/crunchdao/image/upload/v1658148613/250-100-12_syxynz.png);
-    opacity: 0.5;
+    opacity: 0.35;
   }
 
   @keyframes grain {
